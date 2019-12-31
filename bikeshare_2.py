c2b14f6491878149885940fb3c270f5308220be6
Manirou ILAGOUMA <manirou@gmail.com>
2019-12-31 22:18:11 +0100

    Add details to station_stats doc string

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -121,7 +121,10 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """
+    Displays statistics on the most popular stations and trip : Start station, End station.
+
+    """
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
